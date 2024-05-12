550d75e391c582a5676b9a0e1cd718652f782df7
wldlsgur <dhgg321@naver.com>
2024-05-12 19:08:59 +0900

    # 🐣init: 프로젝트 초기 생성

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import { Divider } from './components';
+import './styles/global.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
+    <Divider />
+  </>,
+);
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import * as path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.tsx'),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+  },
+  plugins: [dts()],
+});
