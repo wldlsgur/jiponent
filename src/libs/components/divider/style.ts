b21df9833e4ff7b3ee73fe28149b8fe451572943
wldlsgur <dhgg321@naver.com>
2024-05-18 21:44:58 +0900

    🛠️refactor: divider 컴포넌트 스타일 변수로 관리

--- src/libs/components/divider/style.ts
+++ src/libs/components/divider/style.ts
@@ -1,20 +1,20 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { TDivider } from './type';
+
+const horizontal = css`
+  display: block;
+  width: 100%;
+  height: 1px;
+`;
+const vertical = css`
+  display: inline-block;
+  width: 1px;
+  height: 1rem;
+  vertical-align: middle;
+`;
 
 export const Divider = styled.hr<{ $direction: TDivider }>`
   border: none;
   background-color: #d1d5db;
-  ${({ $direction }) =>
-    $direction === 'horizontal'
-      ? `
-        display: block;
-        width: 100%;
-        height: 1px;
-      `
-      : `
-        display: inline-block;
-        width: 1px;
-        height: 1rem;
-        vertical-align: middle;
-      `}
+  ${({ $direction }) => ($direction === 'horizontal' ? horizontal : vertical)}
 `;
