708d68efe0ed9bd882f39c009cf6f2e4e809961d
지인혁 <dhgg321@naver.com>
2024-05-20 21:51:05 +0900

    🌱feat: 커스텀 훅 내보내기 추가

--- src/libs/index.tsx
+++ src/libs/index.tsx
@@ -7,3 +7,5 @@ export { default as ImageSelect } from '@/libs/components/imageSelect/imageSelec
 // hooks
 export { default as useClickAway } from '@/libs/hooks/useClickAway';
 export { default as useToggle } from '@/libs/hooks/useToggle';
+export { default as useLocalStorage } from '@/libs/hooks/useLocalStorage';
+export { default as useSessionStorage } from '@/libs/hooks/useSessionStorage';
