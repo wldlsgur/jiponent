5331bc1cd335ca3c01869b94bd94abe699789ffc
wldlsgur <dhgg321@naver.com>
2024-06-22 01:43:22 +0900

    🛠️refactor: list 컴포넌트 리팩토링

--- src/libs/components/list/type.ts
+++ src/libs/components/list/type.ts
@@ -1,6 +1,6 @@
-import { HTMLAttributes, ReactNode } from 'react';
+import { ComponentProps, ReactNode } from 'react';
 
-export interface Props<T> extends Omit<HTMLAttributes<HTMLUListElement>, 'children'> {
+export interface Props<T> extends Omit<ComponentProps<'ul'>, 'children'> {
   data: T[];
   children: (data: T, index: number) => ReactNode;
 }
