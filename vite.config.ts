62b765eddd22dd9b99adae476db89996f0723eab
wldlsgur <dhgg321@naver.com>
2024-07-21 01:11:59 +0900

    🔨chore: 린트, 번들 범위 수정 및 css 관련 플러그인 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+import * as path from 'path';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import * as path from 'path';
+import libCss from 'vite-plugin-libcss';
 
 export default defineConfig({
   resolve: {
@@ -11,21 +14,52 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/libs/index.tsx'),
-      name: 'index',
+      name: 'jiponent',
       fileName: 'index',
+      formats: ['es', 'umd', 'cjs'],
     },
     rollupOptions: {
-      external: ['react'],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
           react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
+      plugins: [
+        {
+          name: 'exclude-utils',
+          resolveId(source) {
+            if (source.startsWith(path.resolve(__dirname, 'src/libs/utils/'))) {
+              return { id: source, external: true };
+            }
+            return null;
+          },
+        },
+        {
+          name: 'exclude-stories',
+          transform(_, id) {
+            if (id.endsWith('.stories.tsx')) {
+              return { code: '', map: null };
+            }
+            return null;
+          },
+        },
+      ],
     },
     commonjsOptions: {
       esmExternals: ['react'],
     },
     ssr: true,
   },
-  plugins: [dts()],
+  plugins: [
+    react(),
+    dts({
+      include: 'src/libs/**/*',
+      insertTypesEntry: true,
+      exclude: ['**/*.stories.tsx', 'src/libs/utils/**/*'],
+    }),
+    libCss(),
+    vanillaExtractPlugin(),
+  ],
 });
