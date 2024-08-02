9c056874eea5284d022558a4ac4508897fc7be59
wldlsgur <dhgg321@naver.com>
2024-08-03 02:11:15 +0900

    🛠️refactor: 스토리북 접근성 테스트 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
     '@storybook/addon-actions',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-vite',
